4665fe30a520577fbc163f99a294eedcda97d9ef
Rhys2468 <56052489+Rhys2468@users.noreply.github.com>
2022-08-04 12:28:40 -0700

    Initial creation and testing of the RocketLaunch struct as well as some testing of setting up launch time and dates

--- SpaceX Launches/ViewController.swift
+++ SpaceX Launches/ViewController.swift
@@ -7,10 +7,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct RocketLaunch {
+    var missionName: String = ""
+    var liftOffDate = DateComponents()
+    var launchProvider: String = ""
+    var launchCustomers: [String] = [""]
+    var payloads: [String] = [""]
+}
 
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var Time: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        var launch = RocketLaunch()
+        launch.launchProvider = "SpaceX"
+        launch.launchCustomers = ["NASA", "Intelsat"]
+        launch.payloads = ["Starlink", "Sentinel-6", "JWST"]
+        print(launch.launchProvider + launch.launchCustomers[0] + launch.payloads[0])
+        let now = Date()
+        print("It’s been \(now.timeIntervalSinceReferenceDate) seconds since the start of the Third Millennium.")
+        print(now.description(with: Locale(identifier: "en-US")))
+        let localTimeZoneIdentifier: String = TimeZone.current.identifier
+        print(localTimeZoneIdentifier)
+        Time.text = localTimeZoneIdentifier
+        let launchDate = DateComponents()
         // Do any additional setup after loading the view.
     }
 
