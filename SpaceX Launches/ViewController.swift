587b86d043a698a2b951676219ee919299e5c59b
Rhys2468 <56052489+Rhys2468@users.noreply.github.com>
2022-08-04 15:08:56 -0700

    Completed Launch Site label time being based off of the user's current time zone as well as UTC. Began to include basic info about a launch onto the screen

--- SpaceX Launches/ViewController.swift
+++ SpaceX Launches/ViewController.swift
@@ -8,8 +8,9 @@
 import UIKit
 
 struct RocketLaunch {
-    var missionName: String = ""
-    var liftOffDate = DateComponents()
+    var launchName: String = ""
+    var alternativeLaunchName: String = ""
+    var launchDateAndTime: String = "" //Written in ISO8601 format (2022-08-04T23:08:00+0000)
     var launchProvider: String = ""
     var launchCustomers: [String] = [""]
     var payloads: [String] = [""]
@@ -17,22 +18,43 @@ struct RocketLaunch {
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var Time: UILabel!
+    @IBOutlet weak var ExpectedUTC: UILabel!
+    @IBOutlet weak var ExpectedLocal: UILabel!
+    @IBOutlet weak var Actual: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         var launch = RocketLaunch()
+        launch.launchName = "KPLO"
+        launch.alternativeLaunchName = "Korean Pathfinder Lunar Orbiter"
         launch.launchProvider = "SpaceX"
         launch.launchCustomers = ["NASA", "Intelsat"]
         launch.payloads = ["Starlink", "Sentinel-6", "JWST"]
         print(launch.launchProvider + launch.launchCustomers[0] + launch.payloads[0])
-        let now = Date()
-        print("It’s been \(now.timeIntervalSinceReferenceDate) seconds since the start of the Third Millennium.")
-        print(now.description(with: Locale(identifier: "en-US")))
-        let localTimeZoneIdentifier: String = TimeZone.current.identifier
-        print(localTimeZoneIdentifier)
-        Time.text = localTimeZoneIdentifier
-        let launchDate = DateComponents()
+        
+        let missionName = launch.launchName + " (" + launch.alternativeLaunchName + ")"
+        ExpectedUTC.text = missionName
+        ExpectedUTC.adjustsFontSizeToFitWidth = true
+        
+        
+        let isoDate = "2022-08-04T23:08:00+0000"
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "en-US")
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+        let tempDate = dateFormatter.date(from: isoDate)!
+        
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.year, .month, .day, .hour, .minute, .second], from: tempDate)
+        
+        let localDate = calendar.date(from: components)!
+        dateFormatter.dateFormat = "E, MMMM dd, yyyy h:mm:ss a"
+        var launchDate = dateFormatter.string(from: localDate) + " (" + TimeZone.current.abbreviation()! + ") / "
+        dateFormatter.timeZone = TimeZone(abbreviation: "UTC")
+        dateFormatter.dateFormat = "HH:dd:ss"
+        launchDate += dateFormatter.string(from: localDate) + " (UTC) "
+        ExpectedLocal.text = launchDate
+        ExpectedLocal.adjustsFontSizeToFitWidth = true
         // Do any additional setup after loading the view.
     }
 
