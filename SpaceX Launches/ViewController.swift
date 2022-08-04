e5ab253bc8ace7d30fef255ab3a47a4726644dcd
Rhys2468 <56052489+Rhys2468@users.noreply.github.com>
2022-08-04 16:13:02 -0700

    Created auto resizing labels by adjusting the height of the frame through the use of an extension to the UILabel object

--- SpaceX Launches/ViewController.swift
+++ SpaceX Launches/ViewController.swift
@@ -7,13 +7,14 @@
 
 import UIKit
 
-struct RocketLaunch {
+struct SpaceXLaunch {
     var launchName: String = ""
     var alternativeLaunchName: String = ""
     var launchDateAndTime: String = "" //Written in ISO8601 format (2022-08-04T23:08:00+0000)
-    var launchProvider: String = ""
+    var launchProvider: String = "Space Exploration Technologies Corporation"
+    var launchProviderAbbreviation: String = "SpaceX"
     var launchCustomers: [String] = [""]
-    var payloads: [String] = [""]
+    var launchPayloads: [String] = [""]
 }
 
 class ViewController: UIViewController {
@@ -21,21 +22,20 @@ class ViewController: UIViewController {
     @IBOutlet weak var ExpectedUTC: UILabel!
     @IBOutlet weak var ExpectedLocal: UILabel!
     @IBOutlet weak var Actual: UILabel!
+    @IBOutlet weak var Customers: UILabel!
+    @IBOutlet weak var Payload: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        var launch = RocketLaunch()
+        var launch = SpaceXLaunch()
         launch.launchName = "KPLO"
         launch.alternativeLaunchName = "Korean Pathfinder Lunar Orbiter"
-        launch.launchProvider = "SpaceX"
-        launch.launchCustomers = ["NASA", "Intelsat"]
-        launch.payloads = ["Starlink", "Sentinel-6", "JWST"]
-        print(launch.launchProvider + launch.launchCustomers[0] + launch.payloads[0])
+        launch.launchCustomers = ["Korea Aerospace Research Institute (KARI)", "National Aeronautics and Space Administration (NASA)"]
+        launch.launchPayloads = ["Korean Pathfinder Lunar Orbiter"]
         
         let missionName = launch.launchName + " (" + launch.alternativeLaunchName + ")"
         ExpectedUTC.text = missionName
         ExpectedUTC.adjustsFontSizeToFitWidth = true
-        
         
         let isoDate = "2022-08-04T23:08:00+0000"
         
@@ -55,9 +55,44 @@ class ViewController: UIViewController {
         launchDate += dateFormatter.string(from: localDate) + " (UTC) "
         ExpectedLocal.text = launchDate
         ExpectedLocal.adjustsFontSizeToFitWidth = true
-        // Do any additional setup after loading the view.
+        
+        let rocketProvider = launch.launchProvider + " (" + launch.launchProviderAbbreviation + ")"
+        Actual.text = rocketProvider
+        Actual.adjustsFontSizeToFitWidth = true
+        
+        var missionCustomers: String = ""
+        for customer in launch.launchCustomers{
+            missionCustomers += customer + ", \n"
+        }
+        Customers.text = missionCustomers
+        let customersHeight = Customers.optimalHeight
+        Customers.frame = CGRect(x: Customers.frame.origin.x, y: Customers.frame.origin.y, width: Customers.frame.width, height: customersHeight)
+        Customers.backgroundColor = UIColor.yellow
+        Customers.numberOfLines = 0
+        
+        var missionPayloads: String = ""
+        for payload in launch.launchPayloads{
+            missionPayloads += payload + ", \n"
+        }
+        Payload.text = missionPayloads
+        let payloadsHeight = Customers.optimalHeight
+        Payload.frame = CGRect(x: Payload.frame.origin.x, y: Payload.frame.origin.y, width: Payload.frame.width, height: payloadsHeight)
+        Payload.backgroundColor = UIColor.green
+        Payload.numberOfLines = 0
     }
+}
 
-
+extension UILabel {
+    var optimalHeight: CGFloat {
+        get {
+            let label = UILabel(frame: CGRect(x: 0, y: 0, width: self.bounds.width, height: CGFloat.greatestFiniteMagnitude))
+            label.numberOfLines = 0
+            label.lineBreakMode = .byWordWrapping
+            label.font = self.font
+            label.text = self.text
+            label.sizeToFit()
+            return label.frame.height
+        }
+    }
 }
 
