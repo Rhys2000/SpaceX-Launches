3cdccb2e1bdf707b3d8bed3b929618ae0245270d
Rhys2468 <56052489+Rhys2468@users.noreply.github.com>
2022-08-08 10:01:39 -0700

    Created button that open Launch Sites in Safari browser

---
+++ SpaceX Launches/LaunchCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  LaunchCollectionViewCell.swift
+//  SpaceX Launches
+//
+//  Created by Rhys Julian-Jones on 8/8/22.
+//
+
+import UIKit
+
+class LaunchCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var launchPreviewImage: UIImageView!
+}
