6711ff9d47e6b6abe71ff518587bc6a9df78e940
Rhys2468 <56052489+Rhys2468@users.noreply.github.com>
2022-08-16 16:28:10 -0700

    Xcode is being very strange when I attempt to commit changes but they are showing up on my remote repository

--- SpaceX Launches/Launch.swift
+++ SpaceX Launches/Launch.swift
@@ -30,8 +30,6 @@ struct Launch: Codable {
     let alternativeAbbreviatedLaunchName: String
     let liftOffTime: String
     let customerArray: [String]
-    let something: String
-    let something2: String
     let missionOutcome: Outcome
 }
 
