c7785945f9430fbca77f3045f7123ac82f743c6a
Rhys2468 <56052489+Rhys2468@users.noreply.github.com>
2022-08-22 17:48:38 -0700

    Added another mission milestone stating what number mission it is for SpaceX this year

--- SpaceX Launches/LaunchDetailViewController.swift
+++ SpaceX Launches/LaunchDetailViewController.swift
@@ -13,6 +13,7 @@ class LaunchDetailViewController: UIViewController {
     @IBOutlet weak var secondLabel: UILabel!
     @IBOutlet weak var thirdLabel: UILabel!
     @IBOutlet weak var fourthLabel: UILabel!
+    @IBOutlet weak var fifthLabel: UILabel!
     
     let completeSetOfLaunchData = LaunchLoader().allLaunches
     
@@ -32,6 +33,13 @@ class LaunchDetailViewController: UIViewController {
         //Display what number launch this is from this site
         var launchNumberFromCurrentLaunchSite = 0
         
+        //Display how many launches have happened this year
+        var launchNumberThisYear = 0
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+        let calendar = Calendar.current
+        let currentLaunchYear = calendar.dateComponents([.year], from: dateFormatter.date(from: currentLaunch.liftOffTime)!)
+        
         while(counter < currentLaunch.launchID) {
             if(completeSetOfLaunchData[counter].missionOutcome == .success || completeSetOfLaunchData[counter].missionOutcome == .partialSuccess) {
                 successfulMissionInARow += 1
@@ -48,25 +56,32 @@ class LaunchDetailViewController: UIViewController {
                 launchNumberFromCurrentLaunchSite += 1
             }
             
+            let tempLaunchYear = calendar.dateComponents([.year], from: dateFormatter.date(from: completeSetOfLaunchData[counter].liftOffTime)!)
+            if(currentLaunchYear == tempLaunchYear) {
+                launchNumberThisYear += 1
+            }
+            
             counter += 1
         }
         
-        initialLabel.text = "\(currentLaunch.launchID)" + attachSuffixToNumber(currentValue: currentLaunch.launchID) + " overall launch by SpaceX"
+        initialLabel.text = String(currentLaunch.launchID) + attachSuffixToNumber(currentValue: currentLaunch.launchID) + " overall launch by SpaceX"
         print(initialLabel.text!)
         
         if(currentLaunch.missionOutcome == .success || currentLaunch.missionOutcome == .partialSuccess) {
-            secondLabel.text = "\(successfulMissionInARow)" + attachSuffixToNumber(currentValue: successfulMissionInARow) + " succesful mission in a row for SpaceX"
+            secondLabel.text = String(successfulMissionInARow) + attachSuffixToNumber(currentValue: successfulMissionInARow) + " succesful mission in a row for SpaceX"
         } else {
-            secondLabel.text = "\(totalFailedMission)" + attachSuffixToNumber(currentValue: totalFailedMission) + " failed mission for SpaceX"
+            secondLabel.text = String(totalFailedMission) + attachSuffixToNumber(currentValue: totalFailedMission) + " failed mission for SpaceX"
         }
         print(secondLabel.text!)
         
         //Display what number launch of this vehicle this mission is
-        thirdLabel.text = "\(totalNumberOfLaunchesOfVehicle)" + attachSuffixToNumber(currentValue: totalNumberOfLaunchesOfVehicle) + " launch of the " + currentLaunch.launchVehicle.rawValue + " launch vehicle"
+        thirdLabel.text = String(totalNumberOfLaunchesOfVehicle) + attachSuffixToNumber(currentValue: totalNumberOfLaunchesOfVehicle) + " launch of the " + currentLaunch.launchVehicle.rawValue + " launch vehicle"
         print(thirdLabel.text!)
         
         fourthLabel.text = String(launchNumberFromCurrentLaunchSite) + attachSuffixToNumber(currentValue: launchNumberFromCurrentLaunchSite) + " SpaceX launch from \(currentLaunch.launchLocation.getFullName())"
         print(fourthLabel.text!)
+        
+        fifthLabel.text = String(launchNumberThisYear) + attachSuffixToNumber(currentValue: launchNumberThisYear) + " SpaceX launch attempted this year"
         
         func attachSuffixToNumber(currentValue: Int) -> String {
             if(currentValue == 11 || currentValue == 12 || currentValue == 13) {
