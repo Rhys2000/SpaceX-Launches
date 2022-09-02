bb67c3f48fe4af992a6c6c62150279b363344a97
Rhys2468 <56052489+Rhys2468@users.noreply.github.com>
2022-09-01 19:24:30 -0700

    minor modifications to the default ViewController class

--- SpaceX Launches/ViewController.swift
+++ SpaceX Launches/ViewController.swift
@@ -117,38 +117,56 @@ class ViewController: UIViewController {
         }
         
         
-        var launch = SpaceXLaunch()
-        launch.launchName = "KPLO"
-        launch.alternativeLaunchName = "Korean Pathfinder Lunar Orbiter"
         
-        let missionName = launch.launchName + " (" + launch.alternativeLaunchName + ")"
-        ExpectedUTC.text = missionName
-        ExpectedUTC.adjustsFontSizeToFitWidth = true
         
-        let isoDate = "2022-08-07T23:08:00+0000"
         
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
-        dateFormatter.locale = Locale(identifier: "en-US")
-        tempDate = dateFormatter.date(from: isoDate)!
         
-        let calendar = Calendar.current
-        let components = calendar.dateComponents([.year, .month, .day, .hour, .minute, .second], from: tempDate)
         
-        let localDate = calendar.date(from: components)!
-        dateFormatter.dateFormat = "E, MMMM dd, yyyy h:mm:ss a"
-        var launchDate = dateFormatter.string(from: localDate) + " (" + TimeZone.current.abbreviation()! + ") / "
-        dateFormatter.timeZone = TimeZone(abbreviation: "UTC")
-        dateFormatter.dateFormat = "HH:dd:ss"
-        launchDate += dateFormatter.string(from: localDate) + " (UTC) "
-        ExpectedLocal.text = launchDate
-        ExpectedLocal.adjustsFontSizeToFitWidth = true
         
-        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
         
-        let rocketProvider = launch.launchProvider + " (" + launch.launchProviderAbbreviation + ")"
-        Actual.text = rocketProvider
-        Actual.adjustsFontSizeToFitWidth = true
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//        var launch = SpaceXLaunch()
+//        launch.launchName = "KPLO"
+//        launch.alternativeLaunchName = "Korean Pathfinder Lunar Orbiter"
+//
+//        let missionName = launch.launchName + " (" + launch.alternativeLaunchName + ")"
+//        ExpectedUTC.text = missionName
+//        ExpectedUTC.adjustsFontSizeToFitWidth = true
+//
+//        let isoDate = "2022-08-07T23:08:00+0000"
+//
+//        let dateFormatter = DateFormatter()
+//        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+//        dateFormatter.locale = Locale(identifier: "en-US")
+//        tempDate = dateFormatter.date(from: isoDate)!
+//
+//        let calendar = Calendar.current
+//        let components = calendar.dateComponents([.year, .month, .day, .hour, .minute, .second], from: tempDate)
+//
+//        let localDate = calendar.date(from: components)!
+//        dateFormatter.dateFormat = "E, MMMM dd, yyyy h:mm:ss a"
+//        var launchDate = dateFormatter.string(from: localDate) + " (" + TimeZone.current.abbreviation()! + ") / "
+//        dateFormatter.timeZone = TimeZone(abbreviation: "UTC")
+//        dateFormatter.dateFormat = "HH:dd:ss"
+//        launchDate += dateFormatter.string(from: localDate) + " (UTC) "
+//        ExpectedLocal.text = launchDate
+//        ExpectedLocal.adjustsFontSizeToFitWidth = true
+//
+//        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+//
+//        let rocketProvider = launch.launchProvider + " (" + launch.launchProviderAbbreviation + ")"
+//        Actual.text = rocketProvider
+//        Actual.adjustsFontSizeToFitWidth = true
         
 //        var missionCustomers: String = ""
 //        for customer in launch.launchCustomers{
@@ -170,31 +188,31 @@ class ViewController: UIViewController {
 //        Payload.backgroundColor = UIColor.green
 //        Payload.numberOfLines = 0
         
-        self.timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { [self] _ in updateTime() })
-    }
+//        self.timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { [self] _ in updateTime() })
+    //}
     
-    func updateTime() {
-        let now = Date()
-        let totalSeconds = Int(tempDate - now)
-        let hours = Int(totalSeconds / 3600)
-        var remainingSeconds = totalSeconds % 3600
-        let minutes = Int(remainingSeconds / 60)
-        remainingSeconds = remainingSeconds % 60
-        Countdown.text = "T - " + String(format: "%02d:%02d:%02d", hours, minutes, remainingSeconds)
+//    func updateTime() {
+//        let now = Date()
+//        let totalSeconds = Int(tempDate - now)
+//        let hours = Int(totalSeconds / 3600)
+//        var remainingSeconds = totalSeconds % 3600
+//        let minutes = Int(remainingSeconds / 60)
+//        remainingSeconds = remainingSeconds % 60
+//        Countdown.text = "T - " + String(format: "%02d:%02d:%02d", hours, minutes, remainingSeconds)
     }
 }
 
-extension UILabel {
-    var optimalHeight: CGFloat {
-        get {
-            let label = UILabel(frame: CGRect(x: 0, y: 0, width: self.bounds.width, height: CGFloat.greatestFiniteMagnitude))
-            label.numberOfLines = 0
-            label.lineBreakMode = .byWordWrapping
-            label.font = self.font
-            label.text = self.text
-            label.sizeToFit()
-            return label.frame.height
-        }
-    }
-}
+//extension UILabel {
+//    var optimalHeight: CGFloat {
+//        get {
+//            let label = UILabel(frame: CGRect(x: 0, y: 0, width: self.bounds.width, height: CGFloat.greatestFiniteMagnitude))
+//            label.numberOfLines = 0
+//            label.lineBreakMode = .byWordWrapping
+//            label.font = self.font
+//            label.text = self.text
+//            label.sizeToFit()
+//            return label.frame.height
+//        }
+//    }
+//}
 
