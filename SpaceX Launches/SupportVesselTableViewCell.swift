9e6fa42f107298720c18248cd06028d52f188ca8
Rhys2468 <56052489+Rhys2468@users.noreply.github.com>
2022-09-04 04:41:26 -0700

    Update numerous images in the assets folder, got the UI of the supportVesselTableView looking much nicer and in a presentable state, I added some navigationItem buttons to help make searching through all of the vessels that have been part of the spaceX fleet even easier, added additional functions to give those buttons functionality, prepping for transitioning from a single cell into a detailed view for each vessel that is made programmatically and dynamically

--- SpaceX Launches/SupportVesselTableViewCell.swift
+++ SpaceX Launches/SupportVesselTableViewCell.swift
@@ -15,6 +15,12 @@ class SupportVesselTableViewCell: UITableViewCell {
     private let supportVesselBackgroundView = UIView()
     private let photographerTagLabel = UILabel()
     private let supportVesselNameLabel = UILabel()
+    private let supportVesselServiceLabel = UILabel()
+    private let supportVesselYearsOfServiceLabel = UILabel()
+    private let supportVesselStatusLabel = UILabel()
+    private let supportVesselOperationStatusLabel = UILabel()
+    private let vesselFlagLabel = UILabel()
+    private let supportVesselCountryOfRegistration = UILabel()
     
     public func createSupportVesselPreview(with currentVessel: SupportVessel) {
         
@@ -31,18 +37,66 @@ class SupportVesselTableViewCell: UITableViewCell {
         supportVesselBackgroundView.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
         
         //Photographer Credit settings
-        if(currentVessel.photographerCredit != "") {
-            photographerTagLabel.text = " PC: " + currentVessel.photographerCredit + " "
-        }
+        photographerTagLabel.text = " PC: " + currentVessel.photographerCredit + " "
         photographerTagLabel.backgroundColor = .brown
         photographerTagLabel.font = .systemFont(ofSize: 10)
         photographerTagLabel.sizeToFit()
         photographerTagLabel.layoutIfNeeded()
+        photographerTagLabel.layer.cornerRadius = 3.0
+        photographerTagLabel.layer.masksToBounds = true
         
         //SupportVesselName label settings
-        supportVesselNameLabel.text = "Vessel: " + currentVessel.supportVesselName
+        supportVesselNameLabel.text = currentVessel.supportVesselName
         supportVesselNameLabel.sizeToFit()
         supportVesselNameLabel.layoutIfNeeded()
+        
+        //SupportVesselService label settings
+        supportVesselServiceLabel.text = "Service Record:"
+        supportVesselServiceLabel.font = .boldSystemFont(ofSize: supportVesselServiceLabel.font.pointSize)
+        supportVesselServiceLabel.sizeToFit()
+        supportVesselServiceLabel.layoutIfNeeded()
+        
+        //SupportVesselYearsOfService label Settings
+        var tempService: String = " " + String(currentVessel.yearJoinedSupportFleet)
+        if(currentVessel.yearLeftSupportFleet == 0) {
+            tempService += " - Present"
+        }
+        else if(currentVessel.yearJoinedSupportFleet != currentVessel.yearLeftSupportFleet) {
+            tempService += " - " + String(currentVessel.yearLeftSupportFleet)
+        }
+        supportVesselYearsOfServiceLabel.text = tempService
+        supportVesselYearsOfServiceLabel.sizeToFit()
+        supportVesselYearsOfServiceLabel.layoutIfNeeded()
+        
+        //SupportVesselStatus label setting
+        supportVesselStatusLabel.text = "Status: "
+        supportVesselStatusLabel.font = .boldSystemFont(ofSize: supportVesselStatusLabel.font.pointSize)
+        supportVesselStatusLabel.sizeToFit()
+        supportVesselStatusLabel.layoutIfNeeded()
+        
+        //SupportVesselOperationStatus label settings
+        supportVesselOperationStatusLabel.text = " " + currentVessel.isActive + " "
+        if(currentVessel.isActive == "Active") {
+            supportVesselOperationStatusLabel.backgroundColor = .green
+        } else {
+            supportVesselOperationStatusLabel.backgroundColor = .red
+        }
+        supportVesselOperationStatusLabel.sizeToFit()
+        supportVesselOperationStatusLabel.layoutIfNeeded()
+        supportVesselOperationStatusLabel.layer.cornerRadius = 3.0
+        supportVesselOperationStatusLabel.layer.masksToBounds = true
+        
+        //SupportVesselFlag label settings
+        vesselFlagLabel.text = "Flag:"
+        vesselFlagLabel.font = .boldSystemFont(ofSize: vesselFlagLabel.font.pointSize)
+        vesselFlagLabel.sizeToFit()
+        vesselFlagLabel.layoutIfNeeded()
+        
+        //SupportVesselCountryOfRegistration label settings
+        supportVesselCountryOfRegistration.text = "🇺🇸"
+        supportVesselCountryOfRegistration.sizeToFit()
+        supportVesselCountryOfRegistration.layoutIfNeeded()
+        
     }
     
     override func prepareForReuse() {
@@ -50,25 +104,41 @@ class SupportVesselTableViewCell: UITableViewCell {
         supportVesselImageView.image = nil
         photographerTagLabel.text = nil
         supportVesselNameLabel.text = nil
+        supportVesselYearsOfServiceLabel.text = nil
+        supportVesselOperationStatusLabel.text = nil
+        supportVesselOperationStatusLabel.backgroundColor = .none
     }
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         contentView.addSubview(supportVesselImageView)
         contentView.addSubview(supportVesselBackgroundView)
-        if(photographerTagLabel.frame.width < 26) {
-            contentView.addSubview(photographerTagLabel)
-        }
+        contentView.addSubview(photographerTagLabel)
         contentView.addSubview(supportVesselNameLabel)
-        contentView.backgroundColor = .orange
+        contentView.addSubview(supportVesselServiceLabel)
+        contentView.addSubview(supportVesselYearsOfServiceLabel)
+        contentView.addSubview(supportVesselStatusLabel)
+        contentView.addSubview(supportVesselOperationStatusLabel)
+        contentView.addSubview(vesselFlagLabel)
+        contentView.addSubview(supportVesselCountryOfRegistration)
+        //contentView.backgroundColor = .orange
     }
     
-    override func layoutSubviews() {
+    override func layoutSubviews() { //FIX THE SPACING OF THE TEXT LABELS IN THE UIVIEW
         super.layoutSubviews()
         supportVesselImageView.frame = CGRect(x: 10, y: 10, width: contentView.bounds.width - 20, height: 190)
         supportVesselBackgroundView.frame = CGRect(x: 10, y: supportVesselImageView.frame.maxY, width: contentView.bounds.width - 20, height: 65)
         photographerTagLabel.frame = CGRect(x: 15, y: 15, width: photographerTagLabel.frame.width, height: photographerTagLabel.frame.height)
         supportVesselNameLabel.frame = CGRect(x: 15, y: 205, width: supportVesselNameLabel.frame.width, height: supportVesselNameLabel.frame.height)
+        
+        supportVesselServiceLabel.frame = CGRect(x: 15, y: supportVesselNameLabel.frame.maxY + 10, width: supportVesselServiceLabel.frame.width, height: supportVesselServiceLabel.frame.height)
+        supportVesselYearsOfServiceLabel.frame = CGRect(x: supportVesselServiceLabel.frame.maxX, y: supportVesselNameLabel.frame.maxY + 10, width: supportVesselYearsOfServiceLabel.frame.width, height: supportVesselYearsOfServiceLabel.frame.height)
+        
+        supportVesselOperationStatusLabel.frame = CGRect(x: 375 - supportVesselOperationStatusLabel.frame.width, y: 205, width: supportVesselOperationStatusLabel.frame.width, height: supportVesselOperationStatusLabel.frame.height)
+        supportVesselStatusLabel.frame = CGRect(x: supportVesselOperationStatusLabel.frame.origin.x - supportVesselStatusLabel.frame.width, y: 205, width: supportVesselStatusLabel.frame.width, height: supportVesselStatusLabel.frame.height)
+        
+        supportVesselCountryOfRegistration.frame = CGRect(x: 375 - supportVesselCountryOfRegistration.frame.width, y: supportVesselNameLabel.frame.maxY + 10, width: supportVesselCountryOfRegistration.frame.width, height: supportVesselCountryOfRegistration.frame.height)
+        vesselFlagLabel.frame = CGRect(x: supportVesselCountryOfRegistration.frame.origin.x - vesselFlagLabel.frame.width, y: supportVesselNameLabel.frame.maxY + 10, width: vesselFlagLabel.frame.width, height: vesselFlagLabel.frame.height)
     }
     
     required init?(coder: NSCoder) {
--- SpaceX Launches/ViewController.swift
+++ SpaceX Launches/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     let supportVesselTableView = UITableView()
     let searchController = UISearchController()
     
-    let supportVesselData = SupportVesselLoader().allSupportVessels
+    var supportVesselData = SupportVesselLoader().allSupportVessels
     var filteredSupportVesselData = [SupportVessel]()
     
     override func viewDidLoad() {
@@ -23,6 +23,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         navigationItem.searchController = searchController
         navigationItem.hidesSearchBarWhenScrolling = false
+        navigationItem.leftBarButtonItems = [UIBarButtonItem(image: UIImage(systemName: "arrow.up.circle"), style: .plain, target: self, action: #selector(returnToTop(sender:))), UIBarButtonItem(image: UIImage(systemName: "arrow.down.circle"), style: .plain, target: self, action: #selector(returnToBottom(sender:)))]
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "arrow.up.arrow.down.circle"), style: .plain, target: self, action: #selector(sortArrayAlphabetically(sender:)))
         
         searchController.searchResultsUpdater = self
         searchController.searchBar.scopeButtonTitles = ["All", "Active", "Retired"]
@@ -61,6 +63,12 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         return cell
     }
     
+    
+    
+    
+    
+    
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if(searchController.isActive) {
             print(filteredSupportVesselData[indexPath.row].yearJoinedSupportFleet)
@@ -68,6 +76,12 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             print(supportVesselData[indexPath.row].yearJoinedSupportFleet)
         }
     }
+    
+    
+    
+    
+    
+    
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 275
@@ -90,6 +104,48 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                 return scopeMatch && searchTextMatch
             } else {
                 return scopeMatch
+            }
+        }
+        supportVesselTableView.reloadData()
+    }
+    
+    @objc func returnToTop(sender: UIBarButtonItem) {
+        supportVesselTableView.scrollToRow(at: IndexPath(row: 0, section: 0), at: .top, animated: true)
+    }
+    
+    @objc func returnToBottom(sender: UIBarButtonItem) {
+        if(searchController.isActive) {
+            supportVesselTableView.scrollToRow(at: IndexPath(row: filteredSupportVesselData.count - 1, section: 0), at: .bottom, animated: true)
+        } else {
+            supportVesselTableView.scrollToRow(at: IndexPath(row: supportVesselData.count - 1, section: 0), at: .bottom, animated: true)
+        }
+    }
+    
+    var sortingButtonState: Int = 0
+    @objc func sortArrayAlphabetically(sender: UIBarButtonItem) {
+        if(searchController.isActive) {
+            if(sortingButtonState % 2 == 0) {
+                filteredSupportVesselData = filteredSupportVesselData.sorted {
+                    $0.supportVesselName < $1.supportVesselName
+                }
+                sortingButtonState = 1
+            } else {
+                filteredSupportVesselData = filteredSupportVesselData.sorted {
+                    $0.supportVesselName > $1.supportVesselName
+                }
+                sortingButtonState = 0
+            }
+        } else {
+            if(sortingButtonState % 2 == 0) {
+                supportVesselData = supportVesselData.sorted {
+                    $0.supportVesselName < $1.supportVesselName
+                }
+                sortingButtonState = 1
+            } else {
+                supportVesselData = supportVesselData.sorted {
+                    $0.supportVesselName > $1.supportVesselName
+                }
+                sortingButtonState = 0
             }
         }
         supportVesselTableView.reloadData()
